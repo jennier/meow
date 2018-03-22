3fd646777957bbfac1adc9aa751a92e07ff191b9
jennier <jennieruff@gmail.com>
2018-03-21 22:42:32 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -67,9 +67,9 @@ return [
 		'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'cutcatsc_meow_two'),
-            'username'  => env('DB_USERNAME', 'cutcatsc_meow'),
-            'password'  => env('DB_PASSWORD', 'd6^eDxzdCq8x'),
+            'database'  => env('DB_DATABASE', ''),
+            'username'  => env('DB_USERNAME', ''),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
